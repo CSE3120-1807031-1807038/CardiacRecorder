0af08d28183c88ea7e8701cf5af6bf053fef3cfb
Labib <amirlabibkhan02@gmail.com>
2022-07-06 03:57:41 +0600

    Adding the function of add button

--- app/src/main/java/com/example/cardiacrecorder/MainActivity.java
+++ app/src/main/java/com/example/cardiacrecorder/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.cardiacrecorder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button Add_new_record;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Add_new_record = findViewById(R.id.add_button);
+        Add_new_record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Input_page.class));
+            }
+        });
     }
 }
