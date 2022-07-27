ac2e55987c1b8d665de05d507d992a393610f7aa
Labib <amirlabibkhan02@gmail.com>
2022-07-28 04:08:27 +0600

    Tesing data retrieval on desktop

--- app/src/main/java/com/example/cardiacrecorder/MainActivity.java
+++ app/src/main/java/com/example/cardiacrecorder/MainActivity.java
@@ -1,18 +1,36 @@
 package com.example.cardiacrecorder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     Button Add_new_record;
+    RecyclerView recyclerView;
+    List<Values> valuesList;
+    Database DB;
+    MyAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DB = new Database(this);
+        valuesList = new ArrayList<>();
+        recyclerView = findViewById(R.id.recyclerview_mainpage);
+        adapter = new MyAdapter(this,valuesList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        displaydata();
         Add_new_record = findViewById(R.id.add_button);
         Add_new_record.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -20,5 +38,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(),Input_page.class));
             }
         });
+    }
+
+    private void displaydata() {
+        Cursor cursor = DB.getdata();
+        if(cursor.moveToFirst()){
+            while(!cursor.isAfterLast()){
+                Values values = new Values(cursor.getString(1),cursor.getString(2),
+                        cursor.getString(3),cursor.getString(4),cursor.getString(5),cursor.getString(6));
+                valuesList.add(values);
+                cursor.moveToNext();
+            }
+        }
+        cursor.close();
     }
 }
--- app/src/main/java/com/example/cardiacrecorder/MyAdapter.java
+++ app/src/main/java/com/example/cardiacrecorder/MyAdapter.java
@@ -1,6 +1,7 @@
 package com.example.cardiacrecorder;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,18 +30,37 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Values data = valuesList.get(position);
+        holder.sp_id.setText(data.getS_pressure());
+        int temp = Integer.parseInt(holder.sp_id.getText().toString());
+        if(temp<90 || temp>140){
+            holder.sp_id.setTextColor(Color.RED);
+        }
+        holder.dp_id.setText(data.getD_pressure());
+        temp=Integer.parseInt(holder.dp_id.getText().toString());
+        if(temp<60 || temp>90){
+            holder.dp_id.setTextColor(Color.RED);
+        }
+        holder.bpm_id.setText(data.getHeart_rate());
+        holder.date_id.setText(data.getDate());
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return valuesList.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        TextView sp_id,dp_id,bpm_id,date_id;
+        TextView sp_id,dp_id,bpm_id,date_id,time_id;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            sp_id = itemView.findViewById(R.id.spShow);
+            dp_id = itemView.findViewById(R.id.dpShow);
+            bpm_id = itemView.findViewById(R.id.bpmShow);
+            date_id = itemView.findViewById(R.id.dateShow);
+            time_id = itemView.findViewById(R.id.timeShow);
         }
     }
 }
